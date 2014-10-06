b11711bbc084306569aa4d68ea3d68e104575763
Noah-T <Nteshu@gmail.com>
2014-10-06 17:48:36 -0400

    Initial Commit

---
+++ WeShould/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeShould
+//
+//  Created by Noah Teshu on 10/6/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeShould/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeShould
+//
+//  Created by Noah Teshu on 10/6/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
